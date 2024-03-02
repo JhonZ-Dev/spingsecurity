1825108134ab126e4ab620ad49da8bd248283a97
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-02 17:52:45 -0500

    Update ResourceController.java

--- resouce-server/src/main/java/com/proyecto/resouceserver/controller/ResourceController.java
+++ resouce-server/src/main/java/com/proyecto/resouceserver/controller/ResourceController.java
@@ -1,4 +1,25 @@
 package com.proyecto.resouceserver.controller;
 
+import com.proyecto.resouceserver.dto.MessageDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/resource")
 public class ResourceController {
+
+    @GetMapping("/user")
+    public ResponseEntity<MessageDto> user (Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hola   "+ authentication.getName()));
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<MessageDto> admin (Authentication authentication){
+        return ResponseEntity.ok(new MessageDto("Hola Admin   "+ authentication.getName()));
+    }
 }
