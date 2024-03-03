d80a1321f73f0b9423e893c819b43b68a9f320ae
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-02 20:31:58 -0500

    Update

--- client-angular/src/app/components/authorized/authorized.component.ts
+++ client-angular/src/app/components/authorized/authorized.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-authorized',
   templateUrl: './authorized.component.html',
   styleUrls: ['./authorized.component.css']
 })
-export class AuthorizedComponent {
+export class AuthorizedComponent implements OnInit {
+  code='';
+  
+
+  
+
+  constructor(private activadRoute:ActivatedRoute){}
+  ngOnInit(): void {
+    this.activadRoute.queryParams.subscribe((data: any) => {
+      this.code = data.code;
+    });
+    
+  }
 
 }
