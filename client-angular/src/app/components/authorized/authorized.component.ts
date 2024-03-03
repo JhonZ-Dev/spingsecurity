72e618305aebc3f88580e749bbcb19c5598ae42f
Jhon Zambrano Macias <jhonmacias1999@gmail.com>
2024-03-02 22:03:57 -0500

    Update

--- client-angular/src/app/components/authorized/authorized.component.ts
+++ client-angular/src/app/components/authorized/authorized.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-authorized',
@@ -13,7 +14,7 @@ export class AuthorizedComponent implements OnInit {
 
   
 
-  constructor(private activadRoute:ActivatedRoute, private authService: AuthService){}
+  constructor(private activadRoute:ActivatedRoute, private authService: AuthService, private tokenService:TokenService ){}
   ngOnInit(): void {
     this.activadRoute.queryParams.subscribe((data: any) => {
       this.code = data.code;
@@ -23,7 +24,13 @@ export class AuthorizedComponent implements OnInit {
   }
   getToken(){
     this.authService.getToken(this.code).subscribe(
-      data=>{console.log(data);},err=>{console.log(err);}
+      data=>{
+        //console.log(data);
+        this.tokenService.setTokens(data.access_token, data.refresh_token);
+      },
+      err=>{
+        console.log(err);
+      }
     )
   }
 
---
+++ resouce-server/src/main/java/com/proyecto/resouceserver/config/BeansConfig.java
@@ -0,0 +1,4 @@
+package com.proyecto.resouceserver.config;
+
+public class BeansConfig {
+}
